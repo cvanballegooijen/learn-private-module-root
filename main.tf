843c31b445ab49350e702d1a5b0b7a1121f16a05
Cojan van Ballegooijen <40221816+cvanballegooijen@users.noreply.github.com>
2022-10-16 21:04:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.region
 }
 
 
+
 module "s3-webapp" {
   source  = "app.terraform.io/hc-cojan/s3-webapp/aws"
   version = "1.0.0"
-  # insert required variables here
   name    = var.name
   region  = var.region
   prefix  = var.prefix
-
 }
