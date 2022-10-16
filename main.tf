2acb3f101e882c1f52a939ccfb23bdde3af2963b
Cojan van Ballegooijen <40221816+cvanballegooijen@users.noreply.github.com>
2022-10-16 20:34:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = var.region
 }
 
+
+
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/hc-cojan/s3-webapp/aws"
+  version = "1.0.0"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
-  version = "1.0.0"
 }
